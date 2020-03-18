c429c5bb27f559f2533aa01e6bfafc28f805d0eb
juan <damorenor@unal.edu.co>
2020-03-18 07:52:32 -0500

    restart

--- PicasYFijas/src/picasyfijas/PicasYFijas.java
+++ PicasYFijas/src/picasyfijas/PicasYFijas.java
@@ -4,11 +4,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package picasyfijas;
+//ackage picasyfijas;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -24,10 +23,16 @@ public class PicasYFijas {
     
     static AgentePicaFija agente = new AgentePicaFija();
     static int buscar =1;
-    
-    public static void main(String[] args) throws IOException {
-        
-        String [] basePicas = {"0123",
+    static int indiceMayor;
+    static int auxPicas;
+    static int auxFijas;
+    static String [] input;
+    static char [] numeroAux;
+    static boolean init;
+    static int n;
+    static int r;
+    static int j;
+    static String [] basePicas = {"0123",
                                "1234",
                                "2345",
                                "3456",
@@ -38,21 +43,20 @@ public class PicasYFijas {
                                "8901",
                                "9012"
                                 };
-        int indiceMayor=0;
+    private static void percepcion(){
+        indiceMayor=0;
         agente.setPicas(0);
         agente.setFijas(0);
-        int auxPicas=0;
-        int auxFijas=0;
-        String [] input;
-        
-        char [] numeroAux;
+        auxPicas=0;
+        auxFijas=0;
         
         for (int i = 0; i < basePicas.length; i++) {
-            System.out.println("Ingrese el numero de picas segido del numero de fijas, separandolos por una ',' para el siguiente nuemo: "+basePicas[i]);            
-            input = entrada.readLine().split(",");
+            System.out.println("Ingrese el numero de picas seguido del numero de fijas, separandolos por una ',' para el siguiente numero: "+basePicas[i]);            
+            try{input = entrada.readLine().split(",");
             auxPicas=Integer.parseInt(input[0]);
             auxFijas=Integer.parseInt(input[1]);
-            
+            }catch (IOException ioe) {
+                System.out.println("ERROR: " + ioe.getMessage());;}
             if (auxPicas >0 || auxFijas >0) {
                 auxPicas = auxPicas + auxFijas;
                 if (auxPicas > agente.getPicas()) {
@@ -65,22 +69,25 @@ public class PicasYFijas {
         
         agente.setNumero(basePicas[indiceMayor]);
         numeroAux = agente.getNumero().toCharArray();
-        System.out.println("nuero: "+agente.getNumero());
-        
-        int n = 4;                  //Tipos para escoger
-        int r = numeroAux.length;   //Elementos elegidos
-        Perm2(numeroAux, "", n, r);
+        System.out.println("numero: "+agente.getNumero());
+    }
+
+    private static void accion(){
+        n = 4;                  //Tipos para escoger
+        r = numeroAux.length;   //Elementos elegidos
+        try {Perm2(numeroAux, "", n, r);} catch (IOException ioe) {
+                System.out.println("ERROR: " + ioe.getMessage());;}
      
         numeroAux = agente.getNumero().toCharArray();
         n = 0;
-        int i=0;
+        j=0;
         do {
             if (n>9) {
                 n=0;
-                i++;
+                j++;
             }
-            numeroAux[i]= Integer.toString(n).charAt(0);
-            System.out.println("Ingrese el numero de picas segido del numero de fijas, separandolos por una ',' para el siguiente nuemo: "+ String.valueOf(numeroAux));            
+            numeroAux[j]= Integer.toString(n).charAt(0);
+            System.out.println("Ingrese el numero de picas seguido del numero de fijas, separandolos por una ',' para el siguiente numero: "+ String.valueOf(numeroAux));            
             
             try {
                 input = entrada.readLine().split(",");
@@ -111,6 +118,11 @@ public class PicasYFijas {
                         agente.setFijas(auxFijas);
                         agente.setPicas(auxPicas-auxFijas);
                         System.out.println("su numero es  "+ agente.getNumero());
+                        System.out.println("Ingrese alguna palabra, seguida de enter para volver a empezar");
+                        Scanner sc = new Scanner(System.in);
+                        sc.next();
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
                         break;
                     }
                     
@@ -125,14 +137,12 @@ public class PicasYFijas {
             }
             n++;
         } while (true);
-        
     }
-    
-    private static String Perm2(char[] elem, String act, int n, int r) throws IOException {
+
+    public static String Perm2(char[] elem, String act, int n, int r) throws IOException {
         if (n == 0 && buscar>0) {
-//            System.out.println(act);
             String numAux = act.replaceAll(", ", "");
-            System.out.println("Ingrese el numero de picas segido del numero de fijas, separandolos por una ',' para el siguiente nuemo: "+numAux);            
+            System.out.println("Ingrese el numero de picas seguido del numero de fijas, separandolos por una ',' para el siguiente numero: "+numAux);            
             String [] input = entrada.readLine().split(",");
             int auxPicas=Integer.parseInt(input[0]);
             int auxFijas=Integer.parseInt(input[1]);
@@ -156,6 +166,13 @@ public class PicasYFijas {
             }
         }
         return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        while(true){
+            percepcion();
+            accion();
+        }
     }
     
 }
